79666098f09bc9b4206d1593e2f7c105ba5c8228
Abreu7k <abreu.scaramuzzi@gmail.com>
2022-09-25 22:05:55 -0300

    Cadastro-frontend 3.0
    Criação das listas e dos routes linkando o backend com o frontend, botões para editar, excluir, salvar e cancelar,
    TUDO FEITO

--- src/Main/App.jsx
+++ src/Main/App.jsx
@@ -3,13 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import'font-awesome/css/font-awesome.min.css'
 import React from 'react'
 
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './Routes'
 import Nav from '../components/Nav'
-import Home from '../components/Home/Home'
 import Footer from '../components/Footer'
 
 export default props =>
-<div className='app'>
-    <Nav />
-    <Home />
-    <Footer />
-</div>
+<BrowserRouter>
+    <div className='app'>
+        <Nav />
+        <Routes />
+        <Footer />
+    </div>
+</BrowserRouter>
